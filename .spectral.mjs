5a533f6c93f1d4269a5bc076743a3061b8363527
kendall.h@symsoftsolutions.com <kendall.h@symsoftsolutions.com>
2023-10-12 14:07:04 -0700

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyMjY0OQ/spectral.js?branch_slug=main&token=8de64942-612a-4cb4-997b-e6b0d6ce2f16";
+export default { extends: ruleset };
